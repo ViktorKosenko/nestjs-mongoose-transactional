b466d1af125e9407a0cee7e2b8c5f1ece9a3b7f7
ViktorKosenko <kosenkovv89@gmail.com>
2023-08-11 22:18:51 +0100

    added types to package.json and update tsconfig.json

--- lib/index.ts
+++ lib/index.ts
@@ -1,3 +1,4 @@
 export { sessionStorage } from './storages/transactional-session.storage';
 export { Transactional } from './decorators/transactional.decorator';
 export { AddSessionToLastArguments } from './decorators/add-session-to-last-arguments.decorator';
+export { MongooseTransactionalModule } from './mongoose-transactional.module';
